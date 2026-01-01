3524f3eceb04a872a12095ee5ae25032e377b962
Sunil Kumar <sunil.kksdk@gmail.com>
2026-01-01 23:32:49 +0530

    _

--- Frontend/src/Components/pages/PublicDashboard.jsx
+++ Frontend/src/Components/pages/PublicDashboard.jsx
@@ -2,7 +2,7 @@ import About from "../PublicPages/About.jsx";
 import Contact from "../PublicPages/Contact.jsx";
 import FAQ from "../PublicPages/FAQ.jsx";
 import Footer from "../PublicPages/Footer.jsx";
-import Hero from "../PublicPages/Hero.js";
+import Hero from "../PublicPages/Hero.jsx";
 import Navbar from "../PublicPages/NavBar.jsx";
 import Services from "../PublicPages/Services.jsx";
 
